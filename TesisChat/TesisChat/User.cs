8cd1ce00a2e83e587ec11dacfd40f671da8d1c48
andresrubiop <andresrubiop@msn.com>
2015-06-29 15:29:45 -0500

    Cambios

---
+++ TesisChat/TesisChat/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesisChat
+
+{
+    public class User
+    {
+        private string userName;
+        private string id;
+
+         public User(string userName,string id)
+        {
+            this.userName = userName;
+            this.id = id;
+
+
+        }
+
+         public string UserName
+         {
+             
+             get 
+             {
+                 return this.userName;
+             }
+             set
+             {
+                 userName = value;
+             }
+
+
+         }
+         public string Id
+         {
+             get
+             { 
+                 return this.id;
+             }
+             set 
+             {
+                 id = value;
+             }
+         }
+         public override string ToString()
+         {
+             return this.userName;
+         }
+    }
+}
