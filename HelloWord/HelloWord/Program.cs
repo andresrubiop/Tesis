91b6b06afe47b8a3315fe2fdf502c7b4e9c169d3
andresrubiop <andresrubiop@msn.com>
2015-07-23 13:17:58 -0500

    Graficos

---
+++ HelloWord/HelloWord/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DDS.ConversionUtils;
+using Doopec.Rtps;
+
+using log4net;
+using org.omg.dds.core;
+using org.omg.dds.core.status;
+using org.omg.dds.domain;
+using org.omg.dds.pub;
+using org.omg.dds.sub;
+using org.omg.dds.topic;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
